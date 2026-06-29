fe2a9fa418e25827abdb1894fdeb4366a3d2aa50
Vojtěch <75996784+Vojta188@users.noreply.github.com>
2026-06-29 13:51:46 +0200

    zmena

--- server.js
+++ server.js
@@ -16,9 +16,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3001"
-}));
+app.use(cors());
 app.use(express.json());
 app.use("/api/test", testRoutes);
 app.use("/api/auth", authRoutes);
